a452b2cb5f19189dc7277da6e89223ee0a14dc2e
Aaryan <aaryanrajsarda@gmail.com>
2020-10-23 17:36:28 +0530

    Gestures

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -9,6 +9,8 @@ import {
   TextInput,
   Modal,
   Button,
+  Alert,
+  PanResponder,
 } from "react-native";
 import { Card, Icon, Rating } from "react-native-elements";
 import { connect } from "react-redux";
@@ -47,10 +49,66 @@ function RenderComments(props) {
 
 function RenderDish(props) {
   const dish = props.dish;
+  const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+    if (dx < -200) {
+      //recognize right to left drag
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: (e, gestureState) => {
+      return true;
+    },
+    onPanResponderGrant: () => {
+      // The gesture has started. Show visual feedback so the user knows what is happening!
+      this.view
+        .rubberBand(1000)
+        .then((endState) =>
+          console.log(endState.finished ? "finished" : "cancelled")
+        );
+    },
+    onPanResponderEnd: (e, gestureState) => {
+      //when gesture ends
+      console.log("pan responder end", gestureState);
+      if (recognizeDrag(gestureState))
+        Alert.alert(
+          "Add Favorite",
+          "Are you sure you wish to add " + dish.name + " to favorite?",
+          [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel Pressed"),
+              style: "cancel",
+            },
+            {
+              text: "OK",
+              onPress: () => {
+                props.favorite
+                  ? console.log("Already favorite")
+                  : props.onPress();
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+
+      return true;
+    },
+  });
 
   if (dish != null) {
     return (
-      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+      <Animatable.View
+        animation="fadeInDown"
+        duration={2000}
+        delay={1000}
+        //reference for rubberband animation
+        ref={(ref) => (this.view = ref)}
+        {...panResponder.panHandlers}
+      >
         <Card featuredTitle={dish.name} image={{ uri: baseUrl + dish.image }}>
           <Text style={{ margin: 10 }}>{dish.description}</Text>
           <View style={styles.icons}>
