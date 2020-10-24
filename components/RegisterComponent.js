b220a94e1a791ba36e60b2926a23aa5e1979d35d
Aaryan <aaryanrajsarda@gmail.com>
2020-10-25 02:23:56 +0530

    Image Manipulator

--- components/RegisterComponent.js
+++ components/RegisterComponent.js
@@ -5,6 +5,8 @@ import { baseUrl } from "../shared/baseUrl";
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
 import * as SecureStore from "expo-secure-store";
+import * as ImageManipulator from "expo-image-manipulator";
+import { Asset } from "expo-asset";
 
 class Register extends Component {
   constructor(props) {
@@ -34,11 +36,22 @@ class Register extends Component {
         aspect: [4, 3],
       });
       if (!capturedImage.cancelled) {
-        this.setState({
-          imageUrl: capturedImage.uri,
-        });
+        this.processImage(capturedImage.uri);
       }
     }
+  };
+
+  processImage = async (imageUri) => {
+    let processedImage = await ImageManipulator.manipulateAsync(
+      imageUri,
+      [
+        {
+          resize: { width: 400, height: 300 },
+        },
+      ],
+      { format: "png" }
+    );
+    this.setState({ imageUrl: processedImage.uri });
   };
 
   handleRegister() {
